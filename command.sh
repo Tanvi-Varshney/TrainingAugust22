48e5deef3dd0c57c1e1efaaaf66a826737416282
Anubhav18200 <anubhav.raj34@gmail.com>
2024-09-02 11:49:36 +0000

    updating

---
+++ command.sh
@@ -0,0 +1,7 @@
+git add .
+sleep 2
+git commit -m "updating"
+sleep 2
+git push origin main
+echo Files pushed to git
+
---
+++ helloworld.c
@@ -0,0 +1,18 @@
+//single line comment
+/* multi line comment
+this is a hello world program
+owner : Tanvi Date : 02-09-2022
+Revision history
+1.name:Tanvi v    date:03-09-2024
+2.nme :
+reason:
+*/
+#include<stdio.h>
+#include<stdlib.h>
+#define PI 3.14
+int main(){
+printf("Hello World!\nTanvi Varshney\n124/2,Govind dev, khurja\n");
+printf("%f",PI);
+return EXIT_SUCCESS;
+}
+
